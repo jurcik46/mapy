6cb0a2b86302ae071f82c32cb26e734755273163
Ján Jurč <jurcik46@gmail.com>
2020-11-22 16:00:55 +0100

    geolokation working

--- src/routes/root.route.mjs
+++ src/routes/root.route.mjs
@@ -2,36 +2,30 @@ import express from "express";
 import  csvParser from "csv-parser";
 import fs from "fs";
 import path from "path";
-import { ConsoleTransportOptions } from "winston/lib/winston/transports";
 
 
 const router = express.Router();
 
-const results = [];
 //** Project path: "/api/main/*" */
 // router.use("/main", mainControler.expo);
 
 
 //** POST: "/api/" */
 router.get("/", function (req, res, next) {
+  let results = [];
+
   // return res.json({
   //       title: `${title}`
   //     });
   // console.log(path.resolve('orders.csv'))
   // ['zeme','psc','mesto','ulice','objednavka']
-  fs.createReadStream('/home/jano/dev/mapa/public/orders.csv', 'utf8')
+  fs.createReadStream('/home/jano/dev/mapa/public/orders.csv', { encoding: 'binary' })
   .pipe(csvParser({ separator: ';' }))
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    
-      console.log(results);
-      res.render('../src/views/main', { data: results});
-
-      // [
-      //   { NAME: 'Daffy Duck', AGE: '24' },
-      //   { NAME: 'Bugs Bunny', AGE: '22' }
-      // ]
+    res.render('../src/views/main', { data: results});
   });
+
 });
 
 
